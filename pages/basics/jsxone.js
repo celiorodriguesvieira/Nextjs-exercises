7f094880b3718382684fc9504c5f268e344514d0
celio <celio.ldn@gmail.com>
2022-01-17 23:07:23 -0300

    Components

---
+++ pages/basics/jsxone.js
@@ -0,0 +1,8 @@
+export default function JSXONE() {
+  const conteudo = (
+    <div>
+      <h1>JSXTWO</h1>
+    </div>
+  );
+  return conteudo;
+}
---
+++ pages/basics/jsxtwo.js
@@ -0,0 +1,7 @@
+export default function JSXTWO() {
+  return (
+    <div>
+      <h1>JSX TWO!</h1>
+    </div>
+  );
+}
